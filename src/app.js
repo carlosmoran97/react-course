a0c338857a575f4eb723397f18877192840f0392
Carlos Moran <carlosmoran.97cr@gmail.com>
2018-07-31 15:19:23 -0600

    babel with webpack

--- src/app.js
+++ src/app.js
@@ -1,15 +1,6 @@
-import terceraEdad, {isAdult, canDrink} from './person.js'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const person = {
-    age: 18
-};
+const template = <p>THIS IS JSX WITH WEBPACK</p>;
 
-console.log('Is adult?', isAdult(person.age));
-console.log('Can drink?', canDrink(person.age));
-console.log('Tercera edad?', terceraEdad(person.age));
-
-// import substract, { add, square } from './utils.js'
-
-// console.log(add(10, 12));
-// console.log(square(21));
-// console.log(substract(10, 2));
+ReactDOM.render(template, document.getElementById("app"));
