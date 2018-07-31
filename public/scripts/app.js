e0cff1bd875504a95d88bdd25fda24dc57422647
Carlos Moran <carlosmoran.97cr@gmail.com>
2018-07-31 00:49:28 -0600

    state

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,38 @@
 "use strict";
+
+var app = {
+    visible: false
+};
+
+var appRoot = document.querySelector("#app");
+
+var toggleVisibility = function toggleVisibility() {
+    app.visible = !app.visible;
+    render();
+};
+
+var render = function render() {
+    var template = React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "h1",
+            null,
+            "Visibility Toggle"
+        ),
+        React.createElement(
+            "button",
+            { onClick: toggleVisibility },
+            app.visible ? "Hide details" : "Show details"
+        ),
+        app.visible && React.createElement(
+            "p",
+            null,
+            "Hey. These are some details you can now see!"
+        )
+    );
+
+    ReactDOM.render(template, appRoot);
+};
+
+render();
