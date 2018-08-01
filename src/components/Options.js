8b03d5fc722b869cdfd2229265065d60364ef342
Carlos Moran <carlosmoran.97cr@gmail.com>
2018-07-31 22:11:37 -0600

    indecision scss

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,11 +3,18 @@ import Option from './Option';
 
 const Options = (props) => (
     <div>
-        {props.options.length === 0 && <p>Please add an option to get started.</p>}
-        <button onClick={props.handleDeleteOptions}>Remove All</button>
+        <div className="widget-header">
+            <h3 className="widget-header__title">Your Options</h3>
+            <button 
+                className="button button--link"
+                onClick={props.handleDeleteOptions}>Remove All
+            </button>
+        </div>
+        {props.options.length === 0 && <p className="widget__message">Please add an option to get started.</p>}
         {
             props.options.map( (option, index) => (
                 <Option
+                    count={index + 1}
                     key={index}
                     optionText={option}
                     handleDeleteOptionSingular={props.handleDeleteOptionSingular}
